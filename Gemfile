df9549905e06e6f7f58854ed69f87875719fb1a1
Ahmed Seref 44179-guneysu@users.noreply.gitlab.com <44179-guneysu@users.noreply.gitlab.com>
2025-10-21 01:21:03 +0300

    docs(devenv.flake.nix): update documentation for devenv configuration options and profiles
    docs: improve documentation for `nixosOptionsDoc` and related functions to clarify usage and behavior
    
    docs: update devenv.nix to use latest jeykll and ruby versions
    feat(devenv.nix): add dev script for bundle exec jekyll serve command
    build(devenv.nix): create nix package for ruby and jeykll
    ci(devenv.nix): enable livereload in jekyll server

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "https://rubygems.org"
+
+# Use the GitHub Pages gem to mimic the exact build environment
+gem "github-pages", group: :jekyll_plugins
+
+# If you are on Ruby 3.0 or later, you may need to add the 'webrick' gem
+# which provides the web server for local serving.
+gem "webrick", "~> 1.7"
---
+++ devenv.nix
@@ -0,0 +1,24 @@
+# jeykll + bundle. create a `dev` script for `bundle exec jekyll serve` command
+# <https://devenv.new/?q=jeykll%20%2B%20bundle.%20create%20a%20%60dev%60%20script%20for%20%60bundle%20exec%20jekyll%20serve%60%20command%20>
+
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.ruby
+  ];
+
+  # https://devenv.sh/scripts/
+  scripts.dev = {
+    exec = "bundle install && bundle exec jekyll serve --livereload";
+    description = "Start jekyll server";
+  };
+
+  # See full reference at https://devenv.sh/reference/options/
+}
+
